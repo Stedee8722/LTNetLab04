03702441ddf8ff0c412440a18d90f2c7df65ffca
Stedee <doanthanhbnh@gmail.com>
2025-12-18 01:49:30 +0700

    uhhh form yes

--- RegistrationForm.Designer.cs
+++ RegistrationForm.Designer.cs
@@ -28,12 +28,230 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "RegistrationForm";
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            txtUsername = new TextBox();
+            textBox1 = new TextBox();
+            dateTimePicker1 = new DateTimePicker();
+            radioButton1 = new RadioButton();
+            radioButton2 = new RadioButton();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Black;
+            label1.Location = new Point(437, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(152, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Customer ID:";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Black;
+            label2.Location = new Point(506, 54);
+            label2.Name = "label2";
+            label2.Size = new Size(83, 32);
+            label2.TabIndex = 1;
+            label2.Text = "Name:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Black;
+            label3.Location = new Point(482, 101);
+            label3.Name = "label3";
+            label3.Size = new Size(107, 32);
+            label3.TabIndex = 2;
+            label3.Text = "Birthday:";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.Black;
+            label4.Location = new Point(443, 190);
+            label4.Name = "label4";
+            label4.Size = new Size(146, 32);
+            label4.TabIndex = 3;
+            label4.Text = "Passport No:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Black;
+            label5.Location = new Point(454, 224);
+            label5.Name = "label5";
+            label5.Size = new Size(135, 32);
+            label5.TabIndex = 4;
+            label5.Text = "Nationality:";
+            label5.Click += label5_Click;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.Black;
+            label6.Location = new Point(492, 147);
+            label6.Name = "label6";
+            label6.Size = new Size(97, 32);
+            label6.TabIndex = 5;
+            label6.Text = "Gender:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.Black;
+            label7.Location = new Point(513, 258);
+            label7.Name = "label7";
+            label7.Size = new Size(76, 32);
+            label7.TabIndex = 6;
+            label7.Text = "Email:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Black;
+            label8.Location = new Point(473, 326);
+            label8.Name = "label8";
+            label8.Size = new Size(116, 32);
+            label8.TabIndex = 7;
+            label8.Text = "Password:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label9.ForeColor = Color.Black;
+            label9.Location = new Point(463, 292);
+            label9.Name = "label9";
+            label9.Size = new Size(126, 32);
+            label9.TabIndex = 8;
+            label9.Text = "Username:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label10.ForeColor = Color.Black;
+            label10.Location = new Point(380, 360);
+            label10.Name = "label10";
+            label10.Size = new Size(209, 32);
+            label10.TabIndex = 9;
+            label10.Text = "Confirm Password:";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtUsername.Location = new Point(595, 6);
+            txtUsername.Name = "txtUsername";
+            txtUsername.PlaceholderText = "Customer ID";
+            txtUsername.Size = new Size(388, 39);
+            txtUsername.TabIndex = 10;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(595, 51);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Name";
+            textBox1.Size = new Size(388, 39);
+            textBox1.TabIndex = 11;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CalendarFont = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dateTimePicker1.Location = new Point(595, 96);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(388, 39);
+            dateTimePicker1.TabIndex = 12;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(638, 145);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(92, 36);
+            radioButton1.TabIndex = 13;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(814, 145);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(116, 36);
+            radioButton2.TabIndex = 14;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // RegistrationForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(995, 447);
+            Controls.Add(radioButton2);
+            Controls.Add(radioButton1);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(textBox1);
+            Controls.Add(txtUsername);
+            Controls.Add(label10);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "RegistrationForm";
+            Text = "RegistrationForm";
+            Load += RegistrationForm_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox txtUsername;
+        private TextBox textBox1;
+        private DateTimePicker dateTimePicker1;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
     }
 }
--- RegistrationForm.cs
+++ RegistrationForm.cs
@@ -16,5 +16,25 @@ namespace LTNetLab04
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RegistrationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
